f9a3b13a5417ab7ed581b8716e43be31fb67a0f5
Felipe Taboada <ftaboada@gmail.com>
2020-09-26 15:04:27 -0300

    cleaning

--- routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express();
-
-router.get("/", (req, res) => {
-  res.status(200).json({ saludo: "Hello, Digital Dreamers" });
-});
-module.exports = router;
